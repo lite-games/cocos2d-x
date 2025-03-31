447eb0b70c6b66d70ea2030a614f1de0a02ab53b
Marek Zdankiewicz <m.zdankiewicz@gecko-lab.com>
2025-03-31 13:24:40 +0200

    Fix opening urls on newer iOS versions

--- cocos/platform/ios/CCApplication-ios.mm
+++ cocos/platform/ios/CCApplication-ios.mm
@@ -134,7 +134,13 @@ bool Application::openURL(const std::string &url)
 {
     NSString* msg = [NSString stringWithCString:url.c_str() encoding:NSUTF8StringEncoding];
     NSURL* nsUrl = [NSURL URLWithString:msg];
-    return [[UIApplication sharedApplication] openURL:nsUrl];
+
+    [[UIApplication sharedApplication]
+     openURL:nsUrl
+     options:[NSDictionary dictionary]
+     completionHandler:nil];
+
+    return true;
 }
 
 void Application::applicationScreenSizeChanged(int newWidth, int newHeight) {
